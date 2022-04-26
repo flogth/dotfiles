c506887337bc192e42ab1d024552a32b15043f83
flodobeutlin <flodobeutlin@mailbox.org>
2022-04-26 11:09:31 +0200

    home: xdg mime

--- home/modules/desktop/xdg.nix
+++ home/modules/desktop/xdg.nix
@@ -18,8 +18,9 @@
 
     mimeApps = {
       enable = true;
-      associations = {
-        added = { "application/pdf" = "org.pwmt.zathura.desktop"; };
+      defaultApplications = {
+        "application/pdf" = [ "org.pwmt.zathura.desktop" ];
+        "image/png" = [ "imv.desktop" ];
       };
     };
 

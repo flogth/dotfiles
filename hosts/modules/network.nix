801a69fdd0eb10e661ffbf75802d82123b3db7e3
flodobeutlin <flodobeutlin@mailbox.org>
2022-05-12 16:36:50 +0200

    switch back to networkmanager

--- hosts/modules/network.nix
+++ hosts/modules/network.nix
@@ -16,52 +16,40 @@ in {
       useDHCP = false;
       interfaces = mergeMap (i: { ${i}.useDHCP = true; }) cfg.interfaces;
 
-      networkmanager.enable = false;
-      useNetworkd = false;
-
-      wireless = {
+      networkmanager = {
         enable = true;
-        userControlled.enable = true;
-        environmentFile = "/etc/secrets/wireless.env";
-        networks = {
-          weeelan.psk = "@PSK_WEEELAN@";
-          WLAN_guthmann.psk = "@PSK_WLAN_guthmann@";
-          eduroam = {
-            auth = ''
-              proto=RSN
-              key_mgmt=WPA-EAP
-              eap=PEAP
-              identity="@ID_EDUROAM@"
-              password=hash:@PW_HASH_EDUROAM@
-              domain_suffix_match="eradius.rrze.uni-erlangen.de"
-              anonymous_identity="anonymous@fau.de"
-              phase1="peaplabel=0"
-              phase2="auth=MSCHAPV2"
-              ca_cert="/etc/ssl/certs/eduroam.crt"
-            '';
-          };
-        };
+        dns = "systemd-resolved";
+        # wifi.powersave
       };
+      # wireless = {
+      #   enable = true;
+      #   userControlled.enable = true;
+      #   environmentFile = "/etc/secrets/wireless.env";
+      #   networks = {
+      #     weeelan.psk = "@PSK_WEEELAN@";
+      #     WLAN_guthmann.psk = "@PSK_WLAN_guthmann@";
+      #     eduroam = {
+      #       auth = ''
+      #         proto=RSN
+      #         key_mgmt=WPA-EAP
+      #         eap=PEAP
+      #         identity="@ID_EDUROAM@"
+      #         password=hash:@PW_HASH_EDUROAM@
+      #         domain_suffix_match="eradius.rrze.uni-erlangen.de"
+      #         anonymous_identity="anonymous@fau.de"
+      #         phase1="peaplabel=0"
+      #         phase2="auth=MSCHAPV2"
+      #         ca_cert="/etc/ssl/certs/eduroam.crt"
+      #       '';
+      #     };
+      #   };
+      # };
       firewall = {
         enable = true;
         allowedTCPPorts = [ 631 ];
         allowedUDPPorts = [ 631 5353 ]; # allow mdns
       };
     };
-    systemd.network = {
-      enable = true;
-      networks = {
-        usb = {
-          matchConfig = { Name = "enp4s0*"; };
-          dhcpV4Config = { RouteMetric = 30; };
-        };
-      };
-    };
-
-    systemd.services.systemd-networkd-wait-online.serviceConfig.ExecStart = [
-      ""
-      "${config.systemd.package}/lib/systemd/systemd-networkd-wait-online --any"
-    ];
 
     services = {
       resolved = {

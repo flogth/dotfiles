447d3a658c1ce219b274dd857bbaf102c00c70fa
flodobeutlin <flodobeutlin@mailbox.org>
2022-07-18 14:00:34 +0200

    emacs: changes for agda and coq

--- home/config/emacs/init.el
+++ home/config/emacs/init.el
@@ -332,11 +332,26 @@
 
 ;;; programming languages ==================================
 ;; agda
+(defun local/agda-faces ()
+  "Adjust faces for 'agda2-mode'."
+  (mapc
+   (lambda (x) (add-to-list 'face-remapping-alist x))
+   '((agda2-highlight-datatype-face              . font-lock-type-face)
+     (agda2-highlight-function-face              . font-lock-type-face)
+     (agda2-highlight-inductive-constructor-face . font-lock-function-name-face)
+     (agda2-highlight-keyword-face               . font-lock-keyword-face)
+     (agda2-highlight-module-face                . font-lock-constant-face)
+     (agda2-highlight-number-face                . nil)
+     (agda2-highlight-postulate-face             . font-lock-type-face)
+     (agda2-highlight-primitive-type-face        . font-lock-type-face)
+     (agda2-highlight-record-face                . font-lock-type-face))))
+
 (setup agda2-mode
   (when (executable-find "agda-mode")
     (load-file (let ((coding-system-for-read 'utf-8))
                  (shell-command-to-string "agda-mode locate"))))
-  (:hook #'local/disable-aggressive-indent))
+  (:hook #'local/disable-aggressive-indent
+         #'local/agda-faces))
 
 ;; apl
 (setup gnu-apl-mode)
@@ -358,7 +373,7 @@
 (setup (:package proof-general)
   (:option proof-splash-enable nil
            proof-three-window-enable t
-           proof-three-window-mode-policy 'vertical)
+           proof-three-window-mode-policy 'smart)
   (:with-mode coq-mode
     (:hook #'local/setup-pg-faces
            #'local/coq-init)))

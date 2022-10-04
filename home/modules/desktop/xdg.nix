685ad6f9d68f4f7ada1d9656cc22b21138398c77
flodobeutlin <flodobeutlin@mailbox.org>
2022-10-04 18:20:53 +0200

    home: xhtml mimetype for firefox

--- home/modules/desktop/xdg.nix
+++ home/modules/desktop/xdg.nix
@@ -20,7 +20,8 @@ in {
       enable = true;
       defaultApplications = {
         "text/html" = "firefox.desktop";
-        "application/x-extension-html"= "firefox.desktop";
+        "application/x-extension-html" = "firefox.desktop";
+        "application/xhtml+xml" = "firefox.desktop";
         "x-scheme-handler/http" = "firefox.desktop";
         "x-scheme-handler/https" = "firefox.desktop";
         "x-scheme-handler/about" = "firefox.desktop";

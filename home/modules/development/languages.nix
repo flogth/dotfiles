b9ff10c6b95257f6e26f007a2f39795ab04cac0c
flogth <flogth@mailbox.org>
2022-10-24 20:48:18 +0200

    home dev: add some ocaml

--- home/modules/development/languages.nix
+++ home/modules/development/languages.nix
@@ -1,72 +1,74 @@
 { pkgs, ... }: {
-  home.packages = with pkgs; [
-    # basics
-    stdenv
-    gdb
-    gnumake
-    ninja
-    meson
+  home.packages = with pkgs;
+    [
+      # basics
+      stdenv
+      gdb
+      gnumake
+      ninja
+      meson
 
-    # apl
-    gnuapl
-    cbqn
+      # apl
+      gnuapl
+      cbqn
 
-    # c
-    clang-tools
-    cmake
+      # c
+      clang-tools
+      cmake
 
-    # common lisp
-    sbcl
+      # common lisp
+      sbcl
 
-    # haskell
-    cabal-install
-    ghc
-    haskell-language-server
-    ormolu
+      # haskell
+      cabal-install
+      ghc
+      haskell-language-server
+      ormolu
 
-    # html
-    html-tidy
-    # js
-    nodejs
+      # html
+      html-tidy
+      # js
+      nodejs
 
-    # prolog
-    scryer-prolog
-    swiProlog
+      # prolog
+      scryer-prolog
+      swiProlog
 
-    # proof assistants
-    coq
-    (agda.withPackages [ agdaPackages.standard-library agdaPackages.cubical ])
+      # proof assistants
+      coq
+      (agda.withPackages [ agdaPackages.standard-library agdaPackages.cubical ])
 
-    # python
-    python3
+      # python
+      python3
 
-    # nix
-    nixfmt
-    rnix-lsp
+      # nix
+      nixfmt
+      rnix-lsp
 
-    # rust
-    rustc
-    cargo
-    cargo-generate
-    rust-analyzer
-    rustfmt
+      # rust
+      rustc
+      cargo
+      cargo-generate
+      rust-analyzer
+      rustfmt
 
-    # shell
-    shellcheck
+      # shell
+      shellcheck
 
-    # scheme
-    guile
-    racket
+      # scheme
+      guile
+      racket
 
-    # wasm
-    wasm-pack
-  ];
+      # wasm
+      wasm-pack
+
+    ] ++ (with pkgs.ocamlPackages; [ pkgs.dune_2 ocaml utop ocaml-lsp ]);
 
   home.file = {
     ".ghc/ghci.conf".source = ../../config/ghc/ghci.conf;
     ".guile".source = ../../config/guile/.guile;
     ".agda/defaults".text = ''
-    ${pkgs.agdaPackages.standard-library.name}
+      ${pkgs.agdaPackages.standard-library.name}
     '';
   };
 

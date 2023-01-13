6d20c2db96f35a11cca6b1d0e76fb3a8077f15c0
flogth <flogth@mailbox.org>
2023-01-13 15:26:16 +0100

    [home] xdg: use evince document viewer

--- home/modules/desktop/xdg.nix
+++ home/modules/desktop/xdg.nix
@@ -26,7 +26,6 @@ in {
         "x-scheme-handler/https" = "firefox.desktop";
         "x-scheme-handler/about" = "firefox.desktop";
         "x-scheme-handler/unknown" = "firefox.desktop";
-        "application/pdf" = "firefox.desktop";
         "image/png" = [ "imv.desktop" ];
       };
     };

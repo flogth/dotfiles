af0ad2a32aa719f5b4d9e38f7f64b7229f38bdeb
flogth <flogth@mailbox.org>
2023-02-10 11:52:11 +0100

    [system] reenable snapper

--- hosts/modules/backups.nix
+++ hosts/modules/backups.nix
@@ -8,19 +8,19 @@
         passwordFile = "/etc/restic/data.pw";
       };
     };
-    # snapper = {
-    #   snapshotInterval = "daily";
-    #   cleanupInterval = "weekly";
-    #   configs = {
-    #     home = {
-    #       subvolume = "/home";
-    #       extraConfig = ''
-    #         ALLOW_USERS="flo"
-    #         TIMELINE_CREATE=yes
-    #         TIMELINE_CLEANUP=yes
-    #       '';
-    #     };
-    #   };
-    # };
+    snapper = {
+      snapshotInterval = "daily";
+      cleanupInterval = "weekly";
+      configs = {
+        home = {
+          subvolume = "/home";
+          extraConfig = ''
+            ALLOW_USERS="flo"
+            TIMELINE_CREATE=yes
+            TIMELINE_CLEANUP=yes
+          '';
+        };
+      };
+    };
   };
 } 

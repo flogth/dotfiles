dc19eeb8bcd554cbac9174a98ad53065da9d6408
flogth <flogth@mailbox.org>
2023-03-05 11:53:57 +0100

    [nix] cachix

--- hosts/modules/nix.nix
+++ hosts/modules/nix.nix
@@ -11,6 +11,7 @@
     '';
     settings = {
       trusted-substituters = [ "https://nix-community.cachix.org" ];
+      substituters = [ "https://nix-community.cachix.org" ];
       trusted-public-keys = [
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       ];

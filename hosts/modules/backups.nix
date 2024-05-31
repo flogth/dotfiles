20c08c854c13166e0b38ac6cc30334adcc6132c9
flogth <flogth@mailbox.org>
2024-05-31 08:44:56 +0200

    [host] rm restic

--- hosts/modules/backups.nix
+++ hosts/modules/backups.nix
@@ -1,13 +1,6 @@
 { config, lib, pkgs, ... }:
 {
   services = {
-    restic.backups = {
-      data = {
-        repository = "sftp:backup@flogth.net:.";
-        paths = [ "/home/flo/data" ];
-        passwordFile = "/etc/restic/data.pw";
-      };
-    };
     snapper = {
       snapshotInterval = "daily";
       cleanupInterval = "weekly";

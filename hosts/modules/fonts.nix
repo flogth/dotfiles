09fa83410ed20e5081505bdfda9254476c48a720
flogth <flogth@mailbox.org>
2024-08-10 08:48:14 +0200

    [hosts] fontDir

--- hosts/modules/fonts.nix
+++ hosts/modules/fonts.nix
@@ -1,6 +1,7 @@
 { pkgs, ... }: {
   fonts = {
     enableDefaultPackages = true;
+    fontDir.enable = true;
     packages = builtins.attrValues {
       inherit (pkgs) fira julia-mono lmodern;
     };

bbeceb3e742c454f56f5c830240d5fd6e9b44542
flogth <flogth@mailbox.org>
2025-02-22 10:09:39 +0100

    [euler] disable tlp

--- hosts/modules/power.nix
+++ hosts/modules/power.nix
@@ -1,9 +1,5 @@
 { ... }: {
   services.power-profiles-daemon.enable = false;
-  services.tlp = {
-    enable = true;
-    settings = { USB_AUTOSUSPEND = 0; };
-  };
   systemd.suppressedSystemUnits =
     [ "systemd-backlight@backlight:acpi_video0.service" ];
 
